5395d3766816682343466981624c58bb2fdfa5fb
Valarie001 <jeronovalarie@gmail.com>
2023-02-16 14:26:43 -0800

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+
+int main (void)
+{
+	printf("size of a char: 5zu byte(s)\n", sizeof(char)):
+	printf("size of an int: %zu byte(s)\n", sizeof(int));
+	return(0)
+}
