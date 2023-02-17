4d766c23acfd61b23bfe6ad5109c4f4c0f59a8a2
Valarie001 <jeronovalarie@gmail.com>
2023-02-17 06:55:16 -0800

    task 10

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (succcess)
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dorea Korpar, 2015-10-19\n", 59);
+	return (1);
+}
